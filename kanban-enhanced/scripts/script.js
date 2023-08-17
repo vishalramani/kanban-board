5fd8de7d32090407b567260b8628626ce804296d
Vishal Ramani <vishalramani91@gmail.com>
2023-08-17 12:31:31 +0530

    updated drag & drop along with tooltip

--- kanban-enhanced/scripts/script.js
+++ kanban-enhanced/scripts/script.js
@@ -9,14 +9,24 @@ class kanBanBoard{
         this.mainContainer = document.querySelector('.mainContainer');
         this.allTasks = document.querySelectorAll('.taskContainer');
         this.deleteBtn = document.querySelector('.fa-trash');
+        this.todoContainer = document.querySelector('#todo');
+        this.progressContainer = document.querySelector('#inProgress');
+        this.completedContainer = document.querySelector('#completed');
+        this.resetFilterBtn = document.querySelector('.resetFilter');
+        this.tooltipContainers = document.querySelectorAll('[data-tooltip]');
 
         this.modalStatus = true;
         this.deleteStatus = false;
         this.priorityColor = 'black';
         this.tasksArr = [];
+        this.status = 'todo';
         this.colors = ['red', 'blue', 'green', 'black'];
 
-        this.renderTicketsFromLocalStorage();
+        this.init();
+    }
+
+    init(){
+        this.renderTasksFromLocalStorage();
 
         this.toolBoxContainer.addEventListener('click', this.handleToolBox.bind(this));
         this.toolBoxContainer.addEventListener('dblclick', this.handleResetFilter.bind(this));
@@ -24,15 +34,41 @@ class kanBanBoard{
         this.modalCloseBtn.addEventListener('click', this.toggleModal.bind(this));
         this.deleteBtn.addEventListener('click', this.handleDeleteStatus.bind(this));
         this.textArea.addEventListener('keydown', this.handleTextareaKeyDown.bind(this));
+        this.resetFilterBtn.addEventListener('click', this.handleResetFilter.bind(this));
+
+        this.todoContainer.addEventListener('dragstart', this.handleDragStart.bind(this));
+        this.todoContainer.addEventListener('dragend', this.handleDragEnd.bind(this));
+        
+        this.mainContainer.addEventListener('dragover', this.handleDragOver.bind(this));
+        this.mainContainer.addEventListener('drop', this.handleDrop.bind(this));
 
         for(const colorElm of this.priorityColorElms){
             colorElm.addEventListener('click', this.handleColorSelection.bind(this, colorElm));
         }
+
+        for (const elm of this.tooltipContainers) {
+            // console.log('elm', elm);
+            this.buildToolTip(elm);
+        }
+    }
+
+    buildToolTip(elm){
+        elm.classList.add('tooltip');
+        const data = elm.dataset.tooltip;
+        const position = elm.dataset.tooltipPosition;
+        const tooltipContainer = document.createElement('div');
+        tooltipContainer.className = 'tooltipText';
+        tooltipContainer.classList.add(position);
+        tooltipContainer.innerHTML = `
+            ${data}
+        `;
+        // console.log('elm', elm, 'tooltipContainer', tooltipContainer, 'position', position);
+        
+        elm.appendChild(tooltipContainer);
     }
 
     handleTextareaKeyDown(e){
         if(e.key === 'Enter'){
-            // console.log('enter is clicked');
             const task = this.textArea.value;
             this.createTask(task);
         }
@@ -60,7 +96,7 @@ class kanBanBoard{
     }
 
     handleResetFilter(e){
-        if(e.target.classList.contains('color')){
+        if(e.target.classList.contains('color') || e.target.classList.contains('resetFilter')){
             const allTasks = document.querySelectorAll('.taskColor');
             allTasks.forEach(task => {
                 task.parentElement.style.display = 'block';
@@ -87,6 +123,7 @@ class kanBanBoard{
 
     toggleModal(){
         this.modalContainer.style.display = this.modalStatus ? 'flex' : 'none';
+        if(this.modalStatus) this.textArea.focus();
         this.modalStatus = !this.modalStatus;
     }
 
@@ -120,18 +157,59 @@ class kanBanBoard{
     createTask(content){
         if(content.trim().length <= 0) return;
         const id = new ShortUniqueId().randomUUID();
-        const taskElm = this.createTaskElm(id, content, this.priorityColor);
-        this.mainContainer.appendChild(taskElm);
+        const taskElm = this.createTaskElm(id, content, this.priorityColor, this.status);
+        // this.mainContainer.appendChild(taskElm);
+        this.todoContainer.appendChild(taskElm);
         this.tasksArr.push({
             id,
             task: content,
-            color: this.priorityColor
+            color: this.priorityColor,
+            status: this.status
         });
         this.clearModal();
         this.updateLocalStorage();
         this.handleTaskEdit(id, taskElm);
         this.handleDeleteTask(id, taskElm);
         this.handleTaskColorEdit(id, taskElm);
+
+        // Drag and Drop event
+        taskElm.addEventListener('dragstart', this.handleDragStart);
+        taskElm.addEventListener('dragend', this.handleDragEnd);
+    }
+
+    handleDragStart(e){
+        e.dataTransfer.setData('taskId', e.target.id);
+        e.target.classList.add('dragging');
+    }
+
+    handleDragEnd(e){
+        e.target.classList.remove('dragging');
+    }
+
+    handleDragOver(e){
+        e.preventDefault();
+    }
+
+    handleDrop(e){
+        e.preventDefault();
+        const taskId = e.dataTransfer.getData('taskId');
+        const droppedTask = document.getElementById(taskId);
+        if(droppedTask){
+            const targetContainer = e.target.closest('.task-container');
+            if(targetContainer){
+                const newStatus = targetContainer.id;
+                const sourceContainer = droppedTask.closest('.task-container');
+
+                sourceContainer.removeChild(droppedTask);
+                targetContainer.appendChild(droppedTask);
+
+                const idx = this.tasksArr.findIndex((obj) => obj.id === taskId);
+                if(idx !== -1){
+                    this.tasksArr[idx].status = newStatus;
+                    this.updateLocalStorage();
+                }
+            }
+        }
     }
 
     clearModal(){
@@ -142,6 +220,8 @@ class kanBanBoard{
     createTaskElm(id, content, color){
         const taskContainer = document.createElement('div');
         taskContainer.className = 'taskContainer';
+        taskContainer.draggable = true;
+        taskContainer.id = id;
         taskContainer.innerHTML = `
             <div class="taskColor ${color}"></div>
             <div class="taskId">${id}</div>
@@ -150,6 +230,10 @@ class kanBanBoard{
                 <i class="fa-solid fa-lock"></i>
             </div>
         `;
+
+        // attach drag and drop events
+        taskContainer.addEventListener('dragstart', this.handleDragStart.bind(this));
+        taskContainer.addEventListener('dragend', this.handleDragEnd.bind(this));
         return taskContainer;
     }
 
@@ -195,9 +279,10 @@ class kanBanBoard{
 
     updateLocalStorage(){
         localStorage.setItem('tasks', JSON.stringify(this.tasksArr));
+        localStorage.setItem('taskBoardColors', JSON.stringify(this.colors));
     }
 
-    renderTicketsFromLocalStorage(){
+    renderTasksFromLocalStorage(){
         const storedTasks = JSON.parse(localStorage.getItem('tasks'));
         if(storedTasks){
             this.tasksArr = storedTasks;
@@ -207,16 +292,29 @@ class kanBanBoard{
 
     renderTasksOnLoad(){
         for(const task of this.tasksArr){
-            this.appendTask(task.id, task.task, task.color);
+            this.appendTask(task.id, task.task, task.color, task.status);
         }
     }
 
-    appendTask(id, content, color){
+    appendTask(id, content, color, status){
         const taskElm = this.createTaskElm(id, content, color);
+        taskElm.id = id;
+
+        this.appendTaskElms(taskElm, status);
+
         this.handleTaskEdit(id, taskElm);
         this.handleDeleteTask(id, taskElm);
         this.handleTaskColorEdit(id, taskElm);
-        this.mainContainer.appendChild(taskElm);
+    }
+
+    appendTaskElms(taskElm, status){
+        if(status === 'todo'){
+            this.todoContainer.appendChild(taskElm);
+        } else if(status === 'inProgress'){
+            this.progressContainer.appendChild(taskElm);
+        } else if(status === 'completed'){
+            this.completedContainer.appendChild(taskElm);
+        }
     }
 }
 
